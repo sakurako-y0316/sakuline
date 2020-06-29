c61cf63fc9b088c665537002fe493f53bd0887b4
吉田桜子 <saku3016@icloud.com>
2020-06-30 00:37:21 +0900

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/sakura_line/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sakura_line
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/signup_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
+
+class SignUpScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('新規登録'),
+      ),
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 350,
+          ),
+          Container(
+            color: Colors.brown[200],
+            height: 50,
+            margin: EdgeInsets.only(left: 16, right: 16),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'メールアドレス',
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.brown[200],
+            height: 50,
+            margin: EdgeInsets.only(left: 16, right: 16, top: 15, bottom: 15),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'パスワード',
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            color: Colors.brown[700],
+            height: 50,
+            margin: EdgeInsets.only(left: 16, right: 16, bottom: 15),
+            child: FlatButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute<void>(
+                    builder: (context) {
+                      return HomeScreen();
+                    },
+                  ),
+                );
+              },
+              child: Text(
+                'スキップ',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            color: Colors.brown[700],
+            height: 50,
+            margin: EdgeInsets.only(left: 16, right: 16),
+            child: FlatButton(
+                onPressed: () {},
+                child: Text(
+                  '新規登録',
+                  style: TextStyle(color: Colors.white),
+                )),
+          )
+        ],
+      ),
+    );
+  }
+}
