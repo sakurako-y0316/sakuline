edde3ff4cc8ad8d03d991158abb4b491eec82d05
Sho5Yoshida <1371yoshida@gmail.com>
2020-07-05 14:02:41 +0900

    200705_「新規登録」「ログイン」にSingleChildScrollViewの追加

--- lib/screen/signup_screen.dart
+++ lib/screen/signup_screen.dart
@@ -9,68 +9,70 @@ class SignUpScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('新規登録'),
       ),
-      body: Column(
-        children: <Widget>[
-          SizedBox(
-            height: 350,
-          ),
-          Container(
-            color: Colors.brown[200],
-            height: 50,
-            margin: EdgeInsets.only(left: 16, right: 16),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'メールアドレス',
-                border: InputBorder.none,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 350,
+            ),
+            Container(
+              color: Colors.brown[200],
+              height: 50,
+              margin: EdgeInsets.only(left: 16, right: 16),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'メールアドレス',
+                  border: InputBorder.none,
+                ),
               ),
             ),
-          ),
-          Container(
-            color: Colors.brown[200],
-            height: 50,
-            margin: EdgeInsets.only(left: 16, right: 16, top: 15, bottom: 15),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'パスワード',
-                border: InputBorder.none,
+            Container(
+              color: Colors.brown[200],
+              height: 50,
+              margin: EdgeInsets.only(left: 16, right: 16, top: 15, bottom: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'パスワード',
+                  border: InputBorder.none,
+                ),
               ),
             ),
-          ),
-          Container(
-            width: double.infinity,
-            color: Colors.brown[700],
-            height: 50,
-            margin: EdgeInsets.only(left: 16, right: 16, bottom: 15),
-            child: FlatButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute<void>(
-                    builder: (context) {
-                      return TopScreen();
-                    },
-                  ),
-                );
-              },
-              child: Text(
-                'スキップ',
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-          ),
-          Container(
-            width: double.infinity,
-            color: Colors.brown[700],
-            height: 50,
-            margin: EdgeInsets.only(left: 16, right: 16),
-            child: FlatButton(
-                onPressed: () {},
+            Container(
+              width: double.infinity,
+              color: Colors.brown[700],
+              height: 50,
+              margin: EdgeInsets.only(left: 16, right: 16, bottom: 15),
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute<void>(
+                      builder: (context) {
+                        return TopScreen();
+                      },
+                    ),
+                  );
+                },
                 child: Text(
-                  '新規登録',
+                  'スキップ',
                   style: TextStyle(color: Colors.white),
-                )),
-          )
-        ],
+                ),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              color: Colors.brown[700],
+              height: 50,
+              margin: EdgeInsets.only(left: 16, right: 16),
+              child: FlatButton(
+                  onPressed: () {},
+                  child: Text(
+                    '新規登録',
+                    style: TextStyle(color: Colors.white),
+                  )),
+            )
+          ],
+        ),
       ),
     );
   }
